d1e33728043ee4c62568752a5254cad331b6a3c8
Keonseong <tttzkq9336@gmail.com>
2023-11-30 00:04:03 +0900

    first commit

---
+++ Seat_shuffler/src/main/java/FISA/CE/Seat_shuffler/SeatShufflerApplication.java
@@ -0,0 +1,13 @@
+package FISA.CE.Seat_shuffler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatShufflerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatShufflerApplication.class, args);
+	}
+
+}
