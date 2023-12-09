1a201c7491c71a45aad44046ccccae2b62a062cd
nowead <nowead814@gmail.com>
2023-12-09 23:48:46 +0900

    HomeController 추가, main.html 첫 화면 설정 완료
    할당된 포트로 접속 시 main.html이 뜨지 않아 HomeController 생성하여 접속 시 바로 main.html로 연결되도록 설정함. 하지만 여전히 main화면이 뜨지 않았고 mustache파일이 아닌 경우 application.properties에 설정을 추가해야 하여 spring.mustache.suffix: .html 추가하였음.
    
    main.html 연결은 정상적으로 됐으나 mustache에서 인코딩 시 한글이 깨져서 나오는 오류가 있었고, 오류 해결을 위해 application.properties에 server.servlet.encoding.force=true 추가하였음.

---
+++ Seat_shuffler/src/main/java/FISA/CE/Seat_shuffler/controller/HomeController.java
@@ -0,0 +1,15 @@
+package FISA.CE.Seat_shuffler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "main";
+    }
+    
+}
