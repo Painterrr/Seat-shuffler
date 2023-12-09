87a02e1f60cd4766d131db3bdaade4191420b556
nowead <nowead814@gmail.com>
2023-12-10 01:37:49 +0900

    고정 테이블에서 동적 테이블 생성으로 변경. 테이블 생성 인덱스 오류 및 파라미터 오류 해결.
    동적 테이블 생성을 위해 main.html 을 mustache 문법에 맞게 변경하였고, table이 동적으로 입력될 수 있도록 TableController에서 model.addAttribute("table", table); 추가하여 생성된 값으로 불러오도록 변경.
    
    동적 테이블 생성 설정 후 "Required URI template variable 'row' for method parameter type int is not present row col" 라는 오류가 나와서 createTable 메소드의 파라미터 어노테이션을 @PathVariable에서 @RequestParam으로 변경.
    
    table 생성 시 8, 8을 입력하였더니 java.lang.ArrayIndexOutOfBoundsException: Index 8 out of bounds for length 8 라고 오류가 떴고, TableService의 create 메소드에서 인덱스 0부터 시작으로 가정하여 초기화식과 조건식을 변경하였음.
    
    이전 commit에서 main.html의 경로를 templates/main.html로 바꿔주었기 때문에 createTable의 return 값도 view/main에서 main으로 변경.

--- Seat_shuffler/src/main/java/FISA/CE/Seat_shuffler/controller/TableController.java
+++ Seat_shuffler/src/main/java/FISA/CE/Seat_shuffler/controller/TableController.java
@@ -4,22 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 import FISA.CE.Seat_shuffler.service.TableService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
 public class TableController {
-
     @Autowired
     private TableService tableService = new TableService();
 
     @GetMapping("/table/create") 
-    public String createTable(@PathVariable int row, int col) {
+    public String createTable(@RequestParam int row, @RequestParam int col, Model model) {
         log.info("input row: " + row + "/ col: " + col);
         String[][] table = tableService.create(row, col);
-        return "view/main";
+        model.addAttribute("table", table);
+        return "main";
     }
 
 }
--- Seat_shuffler/src/main/java/FISA/CE/Seat_shuffler/service/TableService.java
+++ Seat_shuffler/src/main/java/FISA/CE/Seat_shuffler/service/TableService.java
@@ -8,8 +8,8 @@ public class TableService {
     public String[][] create(int row, int col) {
         String[][] table = new String[row][col];
 
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= col; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 table[i][j] = "[" + i + "][" + j + "]";
             }
         }
